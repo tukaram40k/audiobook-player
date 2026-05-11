80860771d6eeb61c02904d275cbeff74af5f82c9
tukaram40k <tukaram40k@gmail.com>
2026-05-11 10:24:54 +0300

    add library state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { LibraryProvider } from './state/LibraryProvider'
+import { useLibrary } from './state/useLibrary'
 
-const App: React.FC = () => {
+const LibraryPreview = () => {
+  const { books, currentBook, isLoading, error, openFolder, selectBook } = useLibrary()
+
   return (
-    <h1>Audiobook Player</h1>
+    <div>
+      <h1>Audiobook Player</h1>
+      <button type="button" onClick={() => void openFolder()}>
+        Open Folder
+      </button>
+      {isLoading ? (
+        <p>Loading library...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : (
+        <div>
+          <h2>Books</h2>
+          <ul>
+            {books.map((book) => (
+              <li key={book.id}>
+                <button type="button" onClick={() => selectBook(book.id)}>
+                  {book.title}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <h2>Tracks</h2>
+          {currentBook ? (
+            <ol>
+              {currentBook.tracks.map((track) => (
+                <li key={track.id}>{track.title}</li>
+              ))}
+            </ol>
+          ) : (
+            <p>No book selected.</p>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <LibraryProvider>
+      <LibraryPreview />
+    </LibraryProvider>
   )
 }
 

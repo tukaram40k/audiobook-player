14e21dfa49286337166001c46c4a8cf806c1734f
tukaram40k <tukaram40k@gmail.com>
2026-05-13 02:31:28 +0300

    add folder picker dialog

--- electron/main.ts
+++ electron/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron'
+import { app, BrowserWindow, dialog } from 'electron'
 import path from 'path'
 
 app.whenReady().then(() => {
@@ -10,9 +10,25 @@ app.whenReady().then(() => {
     },
   })
 
+  const pickLibraryFolder = async () => {
+    const result = await dialog.showOpenDialog(win, {
+      title: 'Select audiobook library folder',
+      properties: ['openDirectory'],
+    })
+
+    if (result.canceled || result.filePaths.length === 0) {
+      console.log('[library] selection canceled')
+      return
+    }
+
+    console.log('[library] selected folder:', result.filePaths[0])
+  }
+
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL)
   } else {
     win.loadFile(path.join(__dirname, '../dist/index.html'))
   }
+
+  void pickLibraryFolder()
 })

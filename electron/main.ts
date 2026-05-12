3068c7c079c73f4c2e9187364596c6c06b815c22
tukaram40k <tukaram40k@gmail.com>
2026-05-13 01:40:41 +0300

    add electron to the project

---
+++ electron/main.ts
@@ -0,0 +1,18 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+
+app.whenReady().then(() => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+})

91b857dc3e0ce4d1f84afce130f34d51e3298469
tukaram40k <tukaram40k@gmail.com>
2026-05-13 14:16:51 +0300

    display cover image

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         vite: {
           build: {
             rollupOptions: {
-              external: ['music-metadata'],
+              external: ['iconv-lite', 'music-metadata'],
             },
           },
         },
